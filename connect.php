02e6cea19f26e739b4bf308bbf4b52fa7092c894
abhikamalia <42934925+abhikamalia@users.noreply.github.com>
2019-04-17 16:27:28 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-	$database = mysqli_connect('db4free.net' , 'abhikamalia' , 'abhi@123' , 'github') or die('unable to connect');
+	$database = mysqli_connect('db4free.net' , 'abhikamalia' , 'abhi@123' , 'easy_tummy') or die('unable to connect');
 	echo "connected";
 
 ?>
