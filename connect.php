4ec48632ba645f2a9f49278ec884fb61d315b948
abhikamalia <42934925+abhikamalia@users.noreply.github.com>
2019-04-17 16:06:05 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-	$database = mysqli_connect('localhost' , 'root' , 'abhi@123' , 'easy_tummy');
+	$database = mysqli_connect('localhost' , 'root' , 'abhi@123' , 'easy_tummy') or die('unable to connect');
 	echo "connected";
 
 ?>
